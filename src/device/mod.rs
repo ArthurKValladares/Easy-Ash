f761d053f992be5a5ed81a47718f0e137b7e12b7
ArthurValladares <arthurkaukal@gmail.com>
2022-04-18 21:43:22 -0700

    Create command pool and buffers when creating device

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -14,6 +14,9 @@ pub struct Device {
     pub device: ash::Device,
     pub queue_family_index: u32,
     pub present_queue: vk::Queue,
+    pub command_pool: vk::CommandPool,
+    pub draw_command_buffer: vk::CommandBuffer,
+    pub setup_command_buffer: vk::CommandBuffer,
 }
 
 impl Device {
@@ -70,11 +73,30 @@ impl Device {
 
         let present_queue = unsafe { device.get_device_queue(queue_family_index, 0) };
 
+        let pool_create_info = vk::CommandPoolCreateInfo::builder()
+            .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER)
+            .queue_family_index(queue_family_index);
+
+        let command_pool = unsafe { device.create_command_pool(&pool_create_info, None)? };
+
+        let command_buffer_allocate_info = vk::CommandBufferAllocateInfo::builder()
+            .command_buffer_count(2)
+            .command_pool(command_pool)
+            .level(vk::CommandBufferLevel::PRIMARY);
+
+        let command_buffers =
+            unsafe { device.allocate_command_buffers(&command_buffer_allocate_info)? };
+        let setup_command_buffer = command_buffers[0];
+        let draw_command_buffer = command_buffers[1];
+
         Ok(Self {
             p_device,
             device,
             queue_family_index,
             present_queue,
+            command_pool,
+            draw_command_buffer,
+            setup_command_buffer,
         })
     }
 }
